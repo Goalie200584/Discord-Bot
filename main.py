6cfdbbc4833e45f6b5e1508a129eaf14f1f9d197
Goalie200584 <82061815+Goalie200584@users.noreply.github.com>
2021-04-30 11:36:36 -0400

    main.py
    My main file for my discord bot

---
+++ main.py
@@ -0,0 +1,105 @@
+import os
+import discord
+import requests
+import json
+from keep_alive import keep_alive
+import random
+from discord.ext import commands
+import typing as t
+
+
+client = commands.Bot(command_prefix = '.')
+
+
+
+
+
+evanid = '<@763201251717414942>'
+tylerid = '<@318831677789175808>'
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return quote
+
+@client.event
+async def on_ready():
+  print('We have Logged in as {0.user}' .format(client))
+
+
+
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  
+  if message.content.startswith('/dababy'):
+    await message.channel.send('LESSSS GOOOO')
+
+  if message.content.startswith('/inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+  if message.content.startswith('/idontcare'):
+    await message.channel.send('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+  
+  if message.content.startswith('/evan'):
+    for i in range(2):
+      await message.channel.send (evanid)
+    
+  if message.content.startswith('/tyler'):
+    for i in range(5):
+      await message.channel.send (tylerid)
+
+  if message.content.startswith('/bedgame'):
+    await message.channel.send("@everyone Does anyone wanna play THE BEDGAME?")
+    
+  if message.content.startswith('/pufferboi'):
+     await message.channel.send('https://www.youtube.com/watch?v=qKxfWd79HFI')
+  if message.content.startswith ('/bang'):
+     await message.channel.send ('I WILL bang ur dad ' + tylerid)
+  
+  if message.content.startswith('/bevan'):
+     await message.channel.send('I WILL bang ur dad ' + evanid)
+     
+  if message.content.startswith('/ah ok'):
+     await message.channel.send(tylerid + ' loves foldable dildos')
+
+  if message.content.startswith('/ideas'):
+      await message.channel.send('@everyone What do you want the Bot to do?')
+  
+
+
+@client.command()
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    await channel.connect()
+
+@client.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
+
+
+
+
+
+keep_alive()
+client.run(os.getenv('key')) 
+
+
+
+
+
+#if message.content.startswith('/random'):
+  #  songs = random.randint(1,3)
+   # if (songs == 1):
+  #    await message.channel.send('-play i just had sex')
+  #if (songs == 2):
+   # await message.channel.send ('-play your a wizard harry')
+  #if (songs == 3):
+   # await message.channel.send('-play i got bitches')
+
+
+
